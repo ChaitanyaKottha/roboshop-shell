a115e4d76b1c6d7a4660a5c1c7f0c2190d77bc53
ChaitanyaKottha <chythanya.k@gmail.com>
2023-01-10 07:29:44 +0530

    updating roboshop-shell

---
+++ mongodb.sh
@@ -0,0 +1,22 @@
+source common.sh
+
+print_head "Copying mongodb repo file"
+cp ${script_location}/File/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
+status_check
+
+
+print_head "Install mangodb"
+yum install mongodb-org -y &>>${LOG}
+status_check
+
+print_head "Change 127.0.0.1  to 0.0.0.0 in mongod.conf file"
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>${LOG}
+status_check
+
+print_head "Enable mongodb"
+systemctl enable mongod &>>${LOG}
+status_check
+
+print_head "restart mongodb"
+systemctl restart mongod &>>${LOG}
+status_check
