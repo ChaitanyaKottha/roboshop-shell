2099839302953b0edb8b6bf3b8721a544c4d28ea
ChaitanyaKottha <chythanya.k@gmail.com>
2023-01-11 00:46:32 +0530

    updating roboshop-shell

---
+++ mysql.sh
@@ -0,0 +1,25 @@
+source common.sh
+
+print_head "Disable default my sql 5.7 version"
+dnf module disable mysql -y &>>${LOG}
+status_check
+
+print_head ""
+ &>>${LOG}
+status_check
+
+print_head ""
+ &>>${LOG}
+status_check
+
+print_head ""
+ &>>${LOG}
+status_check
+
+print_head ""
+ &>>${LOG}
+status_check
+
+print_head ""
+ &>>${LOG}
+status_check
