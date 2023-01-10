035b1e9ba7e52c3559d5e84bbef8c8bd21db05d3
ChaitanyaKottha <chythanya.k@gmail.com>
2023-01-10 06:43:50 +0530

    updating roboshop-shell

--- common.sh
+++ common.sh
@@ -3,3 +3,13 @@
 script_location=$(pwd)
 
 LOG=/tmp/roboshop.log
+status_check(){
+  if [$? -eq 0] then
+    echo -e "\e[1;32mSUCCESS\e[0m"
+    else
+      echo -e "\e[1;32mFAILURE\e[0m"
+      echo "Refer log file for more info , LOG - ${LOG}"
+      exit 1
+      fi
+
+}
